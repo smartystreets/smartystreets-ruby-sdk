0bccd9104e9495e55d226a77170abb94f8f12b0b
Neo Pimentel <neo@smartystreets.com>
2016-12-19 16:43:58 -0700

    Added SigningSender.

---
+++ lib/smartystreets_ruby_sdk/signing_sender.rb
@@ -0,0 +1,11 @@
+class SigningSender
+  def initialize(signer, inner)
+    @signer = signer
+    @inner = inner
+  end
+
+  def send(request)
+    @signer.sign(request)
+    @inner.send(request)
+  end
+end
