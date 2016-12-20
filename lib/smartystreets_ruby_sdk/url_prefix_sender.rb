ab6b6921b501b0e3f509a4bb6edfe11d99f88314
Neo Pimentel <neo@smartystreets.com>
2016-12-20 14:56:33 -0700

    Added URLPrefixSender.

---
+++ lib/smartystreets_ruby_sdk/url_prefix_sender.rb
@@ -0,0 +1,12 @@
+class URLPrefixSender
+  def initialize(url_prefix, inner)
+    @url_prefix = url_prefix
+    @inner = inner
+  end
+
+  def send(request)
+    request.url_prefix = @url_prefix
+
+    @inner.send(request)
+  end
+end
