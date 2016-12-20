e9534e534870c13b5c42ab111a29fc0b96c81309
Neo Pimentel <neo@smartystreets.com>
2016-12-20 14:49:53 -0700

    Added SharedCredentials.

---
+++ lib/smartystreets_ruby_sdk/shared_credentials.rb
@@ -0,0 +1,11 @@
+class SharedCredentials
+  def initialize(id, host_name)
+    @id = id
+    @host_name = host_name
+  end
+
+  def sign(request)
+    request.parameters['auth-id'] = @id
+    request.referer = @host_name
+  end
+end
