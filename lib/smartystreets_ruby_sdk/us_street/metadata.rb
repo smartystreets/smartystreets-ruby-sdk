456f12e6837605c2d6d502771e9abee8bd64f4f3
Neo Pimentel <neo@smartystreets.com>
2017-01-03 16:04:14 -0700

    Implemented us_street’s Metadata class.

--- lib/smartystreets_ruby_sdk/us_street/metadata.rb
+++ lib/smartystreets_ruby_sdk/us_street/metadata.rb
@@ -1,6 +1,25 @@
 
 class Metadata
-  def initialize(obj)
+  attr_reader :elot_sort, :longitude, :elot_sequence, :county_fips, :building_default_indicator, :rdi,
+              :congressional_district, :latitude, :precision, :time_zone, :zip_type, :county_name, :utc_offset,
+              :record_type, :carrier_route, :obeys_dst
 
+  def initialize(obj)
+    @record_type = obj['record_type']
+    @zip_type = obj['zip_type']
+    @county_fips = obj['county_fips']
+    @county_name = obj['county_name']
+    @carrier_route = obj['carrier_route']
+    @congressional_district = obj['congressional_district']
+    @building_default_indicator = obj['building_default_indicator']
+    @rdi = obj['rdi']
+    @elot_sequence = obj['elot_sequence']
+    @elot_sort = obj['elot_sort']
+    @latitude = obj['latitude']
+    @longitude = obj['longitude']
+    @precision = obj['precision']
+    @time_zone = obj['time_zone']
+    @utc_offset = obj['utc_offset']
+    @obeys_dst = obj['dst']
   end
 end
