51299138d417fecd0ff97fb8649d52334f101674
Neo Pimentel <neo@smartystreets.com>
2017-01-11 15:44:14 -0700

    Implemented us_zipcode’s ClientBuilder class.

--- lib/smartystreets_ruby_sdk/us_zipcode.rb
+++ lib/smartystreets_ruby_sdk/us_zipcode.rb
@@ -1,5 +1,6 @@
 require_relative './us_zipcode/city'
 require_relative './us_zipcode/client'
+require_relative './us_zipcode/client_builder'
 require_relative './us_zipcode/lookup'
 require_relative './us_zipcode/result'
 require_relative './us_zipcode/zip_code'
