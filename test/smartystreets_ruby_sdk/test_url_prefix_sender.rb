ef346f68b89b3bb740f3dba33f4ad2e83b1dfccc
Neo Pimentel <neo@smartystreets.com>
2017-01-16 10:49:49 -0700

    Wrote tests for URLPrefixSender.

---
+++ test/smartystreets_ruby_sdk/test_url_prefix_sender.rb
@@ -0,0 +1,15 @@
+require './lib/smartystreets_ruby_sdk/url_prefix_sender'
+
+class TestURLPrefixSender < Minitest::Test
+  def test_provided_url_overrides_request_url
+    inner = MockSender.new(Response.new(nil, 123))
+    original = Request.new()
+    original.url_prefix = 'http://www.google.com/the/path/stays'
+    override = 'https://smartystreets.com/the/path/is/ignored'
+    sender = URLPrefixSender.new(override, inner)
+
+    sender.send(original)
+
+    assert_equal(original.url_prefix, override)
+  end
+end
