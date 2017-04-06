fbf202ae2d6aecbb3a35a761321beafca51da227
Neo Pimentel <neo@smartystreets.com>
2017-04-06 16:12:05 -0600

    Implemented extract’s Result.

--- lib/smartystreets_ruby_sdk/us_extract.rb
+++ lib/smartystreets_ruby_sdk/us_extract.rb
@@ -1,6 +1,7 @@
 require_relative './us_extract/lookup'
 require_relative './us_extract/address'
 require_relative './us_extract/metadata'
+require_relative './us_extract/result'
 
 module USExtract
 end
---
+++ lib/smartystreets_ruby_sdk/us_extract/result.rb
@@ -0,0 +1,19 @@
+require_relative './address'
+require_relative './metadata'
+
+module USExtract
+  # See "https://smartystreets.com/docs/cloud/us-extract-api#http-response-status"
+  class Result
+    attr_reader :metadata, :addresses
+
+    def initialize(obj)
+      @metadata = Metadata.new(obj.fetch('meta', {}))
+      addresses = obj.fetch('addresses', [])
+      @addresses = []
+
+      addresses.each { |address|
+        @addresses.push(Address.new(address))
+      }
+    end
+  end
+end
