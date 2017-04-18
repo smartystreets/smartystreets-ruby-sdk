1aade1134d4981e725f138e99abb6a003cc66914
Neo Pimentel <neo@smartystreets.com>
2017-04-18 16:16:02 -0600

    Implemented international Candidate.

--- lib/smartystreets_ruby_sdk/international_street.rb
+++ lib/smartystreets_ruby_sdk/international_street.rb
@@ -2,6 +2,7 @@ require_relative './international_street/lookup'
 require_relative './international_street/metadata'
 require_relative './international_street/analysis'
 require_relative './international_street/components'
+require_relative './international_street/candidate'
 
 module InternationalStreet
 end
