7ffe8016172d2ad590546082c9ae51cfce3855de
Neo Pimentel <neo@smartystreets.com>
2017-04-27 09:17:26 -0600

    Minor refactoring.

--- lib/smartystreets_ruby_sdk/retry_sender.rb
+++ lib/smartystreets_ruby_sdk/retry_sender.rb
@@ -1,6 +1,6 @@
 class RetrySender
   MAX_BACKOFF_DURATION = 10
-  STATUS_OK = '200'
+  STATUS_OK = '200'.freeze
 
   def initialize(max_retries, inner, sleeper, logger)
     @max_retries = max_retries
@@ -12,7 +12,7 @@ class RetrySender
   def send(request)
     response = @inner.send(request)
 
-    (0..@max_retries-1).each { |i|
+    (0..@max_retries-1).each do |i|
       if response.status_code == STATUS_OK
         break
       end
@@ -20,7 +20,7 @@ class RetrySender
       backoff(i)
 
       response = @inner.send(request)
-    }
+    end
 
   response
   end
