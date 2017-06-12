c2e0d0c8f83ab2f2b57337bc9b6a40faf08febce
Neo Pimentel <neo@smartystreets.com>
2017-06-12 14:31:32 -0600

    Added tag.py to help with publishing.

--- lib/smartystreets_ruby_sdk/version.rb
+++ lib/smartystreets_ruby_sdk/version.rb
@@ -1,3 +1,3 @@
 module SmartyStreets
-  VERSION = '0.0.0' # DO NOT EDIT (this is updated by a build job when a new release is published)
+  VERSION = '4.0.1'.freeze # DO NOT EDIT (this is updated by a build job when a new release is published)
 end
