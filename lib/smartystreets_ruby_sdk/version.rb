81666f550dd1da26217ac12793fd9ae9d04d8a8a
Neo Pimentel <neo@smartystreets.com>
2017-06-12 22:21:11 +0000

    Incremented version number to 4.1.3

--- lib/smartystreets_ruby_sdk/version.rb
+++ lib/smartystreets_ruby_sdk/version.rb
@@ -1,3 +1,3 @@
 module SmartyStreets
-  VERSION = '4.1.2' # DO NOT EDIT (this is updated by a build job when a new release is published)
+  VERSION = '4.1.3' # DO NOT EDIT (this is updated by a build job when a new release is published)
 end
