0f49bd9c238052c0199189af4efe512090f7a7ba
Neo Pimentel <neo@smartystreets.com>
2017-06-12 14:21:38 -0600

    Added `address_format` field.

--- lib/smartystreets_ruby_sdk/international_street/metadata.rb
+++ lib/smartystreets_ruby_sdk/international_street/metadata.rb
@@ -3,13 +3,14 @@ module SmartyStreets
     # See "https://smartystreets.com/docs/cloud/international-street-api#metadata"
     class Metadata
 
-      attr_reader :longitude, :geocode_precision, :max_geocode_precision, :latitude
+      attr_reader :longitude, :geocode_precision, :max_geocode_precision, :latitude, :address_format
 
       def initialize(obj)
         @latitude = obj.fetch('latitude', nil)
         @longitude = obj.fetch('longitude', nil)
         @geocode_precision = obj.fetch('geocode_precision', nil)
         @max_geocode_precision = obj.fetch('max_geocode_precision', nil)
+        @address_format = obj.fetch('address_format', nil)
       end
     end
   end
