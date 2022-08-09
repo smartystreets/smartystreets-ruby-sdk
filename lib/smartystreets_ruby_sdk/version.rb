189495b26410427698fa21cdbccec932a1de86cc
Caroline Hickey <caroline@smartystreets.com>
2022-08-09 16:21:37 -0600

    hard coding version test

--- lib/smartystreets_ruby_sdk/version.rb
+++ lib/smartystreets_ruby_sdk/version.rb
@@ -1,3 +1,3 @@
 module SmartyStreets
-  VERSION = '0.0.0' # DO NOT EDIT (this is updated by a build job when a new release is published)
+  VERSION = '5.14.13' # DO NOT EDIT (this is updated by a build job when a new release is published)
 end
