11997382a21dade27da9292eb0f78e5cca16d1f2
Bryan Amundson <bryan@smartystreets.com>
2024-07-02 16:54:10 -0600

    Added new fields for International Autocomplete.

--- lib/smartystreets_ruby_sdk/international_autocomplete/suggestion.rb
+++ lib/smartystreets_ruby_sdk/international_autocomplete/suggestion.rb
@@ -2,12 +2,14 @@ module SmartyStreets
   module InternationalAutocomplete
     class Suggestion
 
-      attr_reader :street, :locality, :administrative_area, :postal_code, :country_iso3, :entries, :address_text, :address_id
+      attr_reader :street, :locality, :administrative_area, :administrative_area_short, :administrative_area_long, :postal_code, :country_iso3, :entries, :address_text, :address_id
 
       def initialize(obj)
         @street = obj.fetch('street', nil)
         @locality = obj.fetch('locality', nil)
         @administrative_area = obj.fetch('administrative_area', nil)
+        @administrative_area_short = obj.fetch('administrative_area_short', nil)
+        @administrative_area_long = obj.fetch('administrative_area_long', nil)
         @postal_code = obj.fetch('postal_code', nil)
         @country_iso3 = obj.fetch('country_iso3', nil)
         # v2 fields
