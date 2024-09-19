75177f8eeee3298298fd817d63b88a5bd122a758
Eric Devenport <eric@Eric-Devenports-MacBook-Air.local>
2024-09-19 17:19:06 -0600

    Removed Development Artifacts

--- lib/smartystreets_ruby_sdk/us_enrichment/lookup.rb
+++ lib/smartystreets_ruby_sdk/us_enrichment/lookup.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require_relative '../json_able'
 module SmartyStreets
   module USEnrichment
@@ -14,17 +13,6 @@ module SmartyStreets
         @city = city
         @state = state
         @zipcode = zipcode
-=======
-module SmartyStreets
-  module USEnrichment
-    class Lookup
-      attr_reader :smarty_key, :data_set, :data_sub_set
-              
-      def initialize(smarty_key, data_set, data_sub_set)
-        @smarty_key = smarty_key
-        @data_set = data_set
-        @data_sub_set = data_sub_set
->>>>>>> 48dda29c9382c5cd28b751e3595b772f6427acc8
       end
     end
   end
