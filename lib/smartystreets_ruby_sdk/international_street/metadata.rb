13489d47dd25a141a6ddc3f62564a8e514c267e3
Jacob McKenzie <159198001+JacobMcKenzieSmarty@users.noreply.github.com>
2025-11-17 12:08:04 -0700

    Add geocode_classification metadata response for intl-street-api (#67)

--- lib/smartystreets_ruby_sdk/international_street/metadata.rb
+++ lib/smartystreets_ruby_sdk/international_street/metadata.rb
@@ -3,12 +3,13 @@ module SmartyStreets
     # See "https://smartystreets.com/docs/cloud/international-street-api#metadata"
     class Metadata
 
-      attr_reader :longitude, :geocode_precision, :max_geocode_precision, :latitude, :address_format, :occupant_use
+      attr_reader :longitude, :geocode_precision, :geocode_classification, :max_geocode_precision, :latitude, :address_format, :occupant_use
 
       def initialize(obj)
         @latitude = obj.fetch('latitude', nil)
         @longitude = obj.fetch('longitude', nil)
         @geocode_precision = obj.fetch('geocode_precision', nil)
+        @geocode_classification = obj.fetch('geocode_classification', nil)
         @max_geocode_precision = obj.fetch('max_geocode_precision', nil)
         @address_format = obj.fetch('address_format', nil)
         @occupant_use = obj.fetch('occupant_use', nil)
