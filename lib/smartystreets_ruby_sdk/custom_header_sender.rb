5ff6a363f787dad26e62cdd16426017a288897a2
Andrew Johnson <andrewjohnson0613@gmail.com>
2026-05-05 14:36:17 -0600

    Modified CustomHeaderSender to combine headers and not overwrite them.

--- lib/smartystreets_ruby_sdk/custom_header_sender.rb
+++ lib/smartystreets_ruby_sdk/custom_header_sender.rb
@@ -7,9 +7,21 @@ module SmartyStreets
     end
 
     def send(request)
-      request.header = @header.transform_values(&:dup)
-      request.append_headers = @append_headers.dup
+      apply_headers.each { |key, value| request.header[key] = value }
       @inner.send(request)
+    end
+
+    def apply_headers
+      result = {}
+      @header.each do |key, values|
+        if @append_headers.key?(key)
+          separator = @append_headers[key]
+          result[key] = values.respond_to?(:join) ? values.join(separator) : values.to_s
+        else
+          result[key] = values.dup
+        end
+      end
+      result
     end
   end
 end
